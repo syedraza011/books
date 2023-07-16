9c3dfee11df972d3f147672c7a38b871926aa714
Syed Raza <syed.raza011@gmail.com>
2023-07-16 12:56:00 -0400

    first push added Firebase package

--- books/ContentView.swift
+++ books/ContentView.swift
@@ -6,8 +6,10 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -24,3 +26,43 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+
+//
+//
+//import SwiftUI
+//import FirebaseCore
+//
+//
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//
+//    return true
+//  }
+//}
+//
+//@main
+//struct YourApp: App {
+//  // register app delegate for Firebase setup
+//  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+//
+//
+//  var body: some Scene {
+//    WindowGroup {
+//      NavigationView {
+//        ContentView()
+//      }
+//    }
+//  }
+//}
