7812ce1100942b4497afde5d54b49d132e6b5b1c
Syed Raza <syed.raza011@gmail.com>
2023-07-16 12:30:37 -0400

    Initial Commit

---
+++ books/booksApp.swift
@@ -0,0 +1,17 @@
+//
+//  booksApp.swift
+//  books
+//
+//  Created by Syed Raza on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct booksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
